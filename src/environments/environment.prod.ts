847c8e4bacced42b8dd802798b96304b6f30bbf4
German Mas <german@creativa77.com.ar>
2022-06-13 17:04:54 -0300

    Add Staging

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  env: 'prod!',
+  anotherVar: 'production variable',
 };
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  env: 'staging',
+  anotherVar: 'staging variable',
+};
