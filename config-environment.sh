2d70d50ac96db626fc72fd468650bea0aea93349
German Mas <german@creativa77.com.ar>
2022-06-13 19:03:26 -0300

    New environment configuration

---
+++ config-environment.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+source .env
+
+NEW_ENV="src/environments/environment-new.ts"
+
+rm -f $NEW_ENV
+touch $NEW_ENV
+
+echo "export const environment = {" >> $NEW_ENV
+echo "production: true," >> $NEW_ENV
+echo "env: \"prod env\"," >> $NEW_ENV
+echo "anotherVar: \"prod var\"," >> $NEW_ENV
+echo "}" >> $NEW_ENV
+
+mv $NEW_ENV "./src/environments/environment.ts"
