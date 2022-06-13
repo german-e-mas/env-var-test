1737447ac5e0497f478c41921c0b7516f0ed8343
German Mas <german@creativa77.com.ar>
2022-06-13 19:23:59 -0300

    Add quotes

--- config-environment.sh
+++ config-environment.sh
@@ -9,8 +9,8 @@ touch $NEW_ENV
 
 echo "export const environment = {" >> $NEW_ENV
 echo "production: true," >> $NEW_ENV
-echo "env: $GER_ENV," >> $NEW_ENV
-echo "anotherVar: $GER_VAR," >> $NEW_ENV
+echo "env: \"$GER_ENV\"," >> $NEW_ENV
+echo "anotherVar: \"$GER_VAR\"," >> $NEW_ENV
 echo "}" >> $NEW_ENV
 
 mv $NEW_ENV "./src/environments/environment.ts"
